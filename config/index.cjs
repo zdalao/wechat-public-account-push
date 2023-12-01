db0fe98aaab3b688e8009537248c1316bf1c02aa
dalao <32741315+zdalao@users.noreply.github.com>
2023-12-01 17:19:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
         {
             "name": "宝子",
             "id": "ostKU6hgy-SFd8XBAIXBN7Z79Z8Y",
-            "useTemplateId": "lfrVpnNo8IqJHsyikT2ym-o29HyAIrgEJa0k6T9huJU",
+            "useTemplateId": "WLbSIdf77a6m5i6kekQ9917_z2trrJJVQLycjnTlO_w",
             "province": "山东",
             "city": "乳山",
             "horoscopeDate": "10-27",
