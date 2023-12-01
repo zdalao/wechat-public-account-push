9bb154e37aa1f8bb1c555d1f51730ce455123df1
dalao <32741315+zdalao@users.noreply.github.com>
2023-12-01 10:47:31 +0800

    Update index.cjs
    更新配置

--- config/index.cjs
+++ config/index.cjs
@@ -5,63 +5,56 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
+
 const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    "APP_ID": "wx7f51a01ab1e13c77",
+    "APP_SECRET": "5e4d5a5aa9d2fc619611febb7194b842",
+    "IS_SHOW_COLOR": false,
+    "CALLBACK_TEMPLATE_ID": "PKO-bNWbIyl4-nvottZ6LJjKr8mamNULIxCF42jmikE",
+    "CALLBACK_USERS": [
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+            "name": "自己",
+            "id": "ostKU6hgy-SFd8XBAIXBN7Z79Z8Y\t"
+        }
+    ],
+    "USERS": [
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+            "name": "宝子",
+            "id": "ostKU6hgy-SFd8XBAIXBN7Z79Z8Y",
+            "useTemplateId": "IrQhRZWIfHY1TDa6E0fIVTH7vkPFcf0zomQ2bR3WHrU",
+            "province": "山东",
+            "city": "乳山",
+            "horoscopeDate": "10-27",
+            "horoscopeDateType": "今日",
+            "openUrl": "www.baidu.com",
+            "festivals": [
+                {
+                    "type": "*生日",
+                    "name": "宝子",
+                    "date": "10-27",
+                    "year": "1994"
+                }
+            ],
+            "customizedDateList": [
+              
+            ]
+        }
+    ],
+    "SWITCH": {
+        "weather": true,
+        "holidaytts": true,
+        "CIBA": false,
+        "oneTalk": true,
+        "earthyLoveWords": false,
+        "momentCopyrighting": false,
+        "poisonChickenSoup": false,
+        "poetry": false,
+        "horoscope": true,
+        "birthdayMessage": true
     }
-  ],
-
 }
+
+
 
 module.exports = USER_CONFIG
 
